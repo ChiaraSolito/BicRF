210db4c6cd3efe91e5d51093f38f4a35b3a3ae63
ChiaraSolito <chiara.solito00@gmail.com>
2024-07-12 17:09:14 +0200

    correct metrics

--- src/inverse_purity.m
+++ src/inverse_purity.m
@@ -1,7 +1,9 @@
 function inverse_purity = inverse_purity(A1,R1,A2,R2)
+%1 = Original Bicluster
+%2 = Found Bicluster
     rows = length(intersect(R1,R2));
     cols = length(intersect(A1,A2));
 
-    inverse_purity = (rows*cols)/(length(R2)*length(A2));
+    inverse_purity = (rows*cols)/(length(R1)*length(A1));
 end
 
--- src/purity.m
+++ src/purity.m
@@ -1,7 +1,9 @@
 function purity = purity(A1,R1,A2,R2)
+%1 = Original Bicluster
+%2 = Found Bicluster
     cols = length(intersect(A1,A2));
     rows = length(intersect(R1, R2));
     
-    purity = (rows*cols)/(length(A1)*length(R1));
+    purity = (rows*cols)/(length(A2)*length(R2));
 end
 
